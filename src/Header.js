c95899cdc17b3c6547e54821c63c3a5a44e8f87d
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-07-12 17:20:26 +0600

    added initial design

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+function Header() {
+    return (
+        <head>
+            <meta charSet="utf-8"/>
+                <title>bs4 todo list--sajib</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+                          rel="stylesheet"/>
+                        <script
+                            src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+        </head>
+    );
+}
+export default Header
