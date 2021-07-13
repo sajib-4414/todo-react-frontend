88a2bf3de613ce07744fa62f824a192f7bbbf470
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-07-13 14:56:29 +0600

    check uncheck with crossing out done

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../designs/Todo.css'
 function Todo({todo,notifyCompleteStatusChange}) {
     function handleToggle(){
         // todo.completed = !todo.completed
@@ -7,8 +8,9 @@ function Todo({todo,notifyCompleteStatusChange}) {
 
     return (
         <div className="todo-item">
-            <div className="checker"><span className=""><input type="checkbox" onChange={handleToggle}/></span></div>
-            <span>{todo.description}</span>
+            <div className="checker"><span className=""><input type="checkbox" checked={!todo.completed?null:"true"}
+                                                               onChange={handleToggle}/></span></div>
+            <span className={todo.completed?"text-strike":null}>{todo.description}</span>
             {/*<a  className="float-right remove-todo-item">*/}
                 <button type="button" className="float-right btn-close" aria-label="Close">X</button>
         {/*</a>*/}
