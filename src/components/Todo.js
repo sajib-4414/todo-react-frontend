c12b4e9a151a67c1f7ed9ac0b7841f5c59eadf73
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-07-13 22:43:14 +0600

    in middle of finishing edit functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React,{ useState } from "react";
 import '../designs/Todo.css'
 import { confirmAlert } from 'react-confirm-alert'; // Import
 import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
-function Todo({todo,notifyCompleteStatusChange, notifyItemDelete}) {
+function Todo({todo,notifyCompleteStatusChange, notifyItemDelete, itemEditCallBack}) {
+    const [editMode, setEditMode] = useState(false);
+
 
     const options = {
         title: 'Confirm delete?',
@@ -38,14 +40,47 @@ function Todo({todo,notifyCompleteStatusChange, notifyItemDelete}) {
         confirmAlert(options);
 
     }
+    function handleEditCancelButtonClick(){
+        // alert("Hi I am here")
+        if (!editMode){
+            setEditMode(!editMode)
+            //toggle edit now
+            //show an edit form
+        }
+        else{
+            //turn off edit
+            setEditMode(!editMode)
+        }
+    }
+    function handleFormSubmit(formEvent){
+        formEvent.preventDefault()
+
+    }
+    // function handleCancelButtonClick(){
+    //
+    // }
 
     return (
         <div className="todo-item">
-            <div className="checker"><span className=""><input type="checkbox" checked={!todo.completed?null:"true"}
-                                                               onChange={handleToggle}/></span></div>
-            <span className={todo.completed?"text-strike":null}>{todo.description}</span>
-            {/*<a  className="float-right remove-todo-item">*/}
+            <div >
+                <div className="checker"><span className=""><input type="checkbox" checked={!todo.completed?null:"true"}
+                                                                   onChange={handleToggle}/></span></div>
+                <span className={todo.completed?"text-strike":null}>{todo.description}</span>
                 <button type="button" onClick={handleDelete} className="float-right btn-close" aria-label="Close">X</button>
+            </div>
+            {editMode?
+                <div>
+                    <form onSubmit={handleFormSubmit}>
+                        <input value={todo.description} placeholder="Enter the edit text"/>
+                        <button type="submit">Submit</button>
+                        <button type="button" onClick={handleEditCancelButtonClick}>Cancel</button>
+                    </form>
+                </div>
+                : <button type="button" onClick={handleEditCancelButtonClick} className=" btn btn-secondary" aria-label="Close">Edit</button>
+            }
+
+
+
         {/*</a>*/}
         </div>
     );
