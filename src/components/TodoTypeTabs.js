623db1df2b697506bce83e9b000fc096fca306aa
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-07-13 12:21:00 +0600

    added items and tab types is now a dynamic array

--- src/components/TodoTypeTabs.js
+++ src/components/TodoTypeTabs.js
@@ -1,14 +1,18 @@
 import React,{Component} from "react";
 
 class TodoTypeTabs extends React.Component{
+    tabs = [
+        "All",
+        "Active",
+        "Completed"
+    ]
     render() {
         return(
-            <React.Fragment>
-                <li role="presentation" className="nav-item all-task active"><a href="#" className="nav-link">All</a></li>
-                <li role="presentation" className="nav-item active-task"><a href="#" className="nav-link">Active</a></li>
-                <li role="presentation" className="nav-item completed-task"><a href="#" className="nav-link">Completed</a></li>
-            </React.Fragment>
-        )
+            this.tabs.map(item => (
+                <li role="presentation" className="nav-item all-task active"><a href="#" className="nav-link">{item}</a></li>
+
+            )))
+
     }
 }
 export default TodoTypeTabs
