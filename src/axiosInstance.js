a3261633923bdf1d0c1a211a25d70653a3b5d5c3
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-07-23 19:25:24 +0600

    Added skeleton interceptor methods

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -5,5 +5,21 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
+//below are the code for interceptors
+//Interceptors allow you to step in before a request is sent or a response is
+// received and execute the code you want or modify the request or response or handle errors.
+axiosInstance.interceptors.response.use(response => {
+    // code to be excuted.
+    return response;
+}, error => {
+
+});
+
+axiosInstance.interceptors.request.use(request => {
+    // code to be executed
+    return request;
+}, error => {
+
+});
 
 export default axiosInstance
